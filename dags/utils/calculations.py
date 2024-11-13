305ac50214db39273fe8c425f90a9af4c2f18d41
arthur-ventura-astro <arthurplautz.ventura@astronomer.io>
2024-11-13 16:01:42 -0300

    Changing default factor

--- dags/utils/calculations.py
+++ dags/utils/calculations.py
@@ -3,7 +3,7 @@ from random import randint
 batches = [i for i in range(5)]
 bathces = [i for i in range(500)]
 
-def compute_factor(factor=100):
+def compute_factor(factor=10):
     c, i = 0, 0
     while c < factor:
         h = hash(randint(0, 10_000_000))
